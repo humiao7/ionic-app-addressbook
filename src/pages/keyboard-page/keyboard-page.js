f9ccd64dd1d8cb45d63023fcec63e67deeae67c2
humiao <1124003166@qq.com>
2017-11-08 16:01:51 +0800

    实现拨号界面退格功能

--- src/pages/keyboard-page/keyboard-page.js
+++ src/pages/keyboard-page/keyboard-page.js
@@ -14,5 +14,17 @@
 
     keyboardVM.pageData = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['*', '0', '#']];
 
+    keyboardVM.callNumber = '';
+
+    keyboardVM.clickButton = clickButton;
+    function clickButton(num) {
+      keyboardVM.callNumber = keyboardVM.callNumber + num;
+    }
+
+    keyboardVM.backSpace = backSpace;
+    function backSpace() {
+      keyboardVM.callNumber = keyboardVM.callNumber.substring(0, keyboardVM.callNumber.length-1);
+    }
+
   }
 })();
