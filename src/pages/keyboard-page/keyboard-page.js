6d34378401229176d8791447b68cf2a9f63eebec
humiao <1124003166@qq.com>
2017-11-09 14:13:55 +0800

    拨打电话记时功能1

--- src/pages/keyboard-page/keyboard-page.js
+++ src/pages/keyboard-page/keyboard-page.js
@@ -7,23 +7,43 @@
     .module('loginModule')
     .controller('keyboardCtrl', keyboardCtrl);
 
-  keyboardCtrl.$inject = [];
+  keyboardCtrl.$inject = ['$window', '$interval'];
 
-  function keyboardCtrl() {
+  function keyboardCtrl($window, $interval) {
     var keyboardVM = this;
-
     keyboardVM.pageData = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['*', '0', '#']];
-
     keyboardVM.callNumber = '';
+    keyboardVM.onCalling = false;
 
-    keyboardVM.clickButton = clickButton;
+    // 方法
+    keyboardVM.backSpace = backSpace; //退格
+    keyboardVM.clickButton = clickButton; //拨号键点击
+    keyboardVM.callPhoneNumber = callPhoneNumber; //拨打电话
+
+    keyboardVM.breakPhone = breakPhone; //挂掉电话
+
     function clickButton(num) {
       keyboardVM.callNumber = keyboardVM.callNumber + num;
     }
 
-    keyboardVM.backSpace = backSpace;
     function backSpace() {
-      keyboardVM.callNumber = keyboardVM.callNumber.substring(0, keyboardVM.callNumber.length-1);
+      keyboardVM.callNumber = keyboardVM.callNumber.substring(0, keyboardVM.callNumber.length - 1);
+    }
+
+    function callPhoneNumber(phone_number) {
+      $window.location.href = "tel:" + phone_number;
+      var times = 0;
+      keyboardVM.onCalling = true;
+      keyboardVM.timer = '00:00:00';
+      var test = $interval(function () {
+        times++;
+        keyboardVM.timer = (parseInt(times / 3600) < 10 ? '0' + parseInt(times / 3600) : parseInt(times / 3600)) + ':' + (parseInt((times % 3600) / 60) < 10 ? '0' + parseInt((times % 3600) / 60) : parseInt((times % 3600) / 60)) + ':' + ((times % 3600) % 60 < 10 ? '0' + (times % 3600) % 60 : (times % 3600) % 60);
+      }, 1000);// 表示每一秒执行一次，执行三次
+    }
+
+    function breakPhone() {
+      console.log('挂断电话');
+      $interval.cancel('test');
     }
 
   }
