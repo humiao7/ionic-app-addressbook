2ecb4fe334cf8423bdfda48f9957888fc12d27b6
humiao <1124003166@qq.com>
2017-11-09 15:26:46 +0800

    添加通话中提示框

--- src/pages/keyboard-page/keyboard-page.js
+++ src/pages/keyboard-page/keyboard-page.js
@@ -7,20 +7,17 @@
     .module('loginModule')
     .controller('keyboardCtrl', keyboardCtrl);
 
-  keyboardCtrl.$inject = ['$window', '$interval'];
+  keyboardCtrl.$inject = ['$window', '$interval', '$ionicPopup'];
 
-  function keyboardCtrl($window, $interval) {
+  function keyboardCtrl($window, $interval, $ionicPopup) {
     var keyboardVM = this;
     keyboardVM.pageData = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['*', '0', '#']];
     keyboardVM.callNumber = '';
-    keyboardVM.onCalling = false;
 
     // 方法
     keyboardVM.backSpace = backSpace; //退格
     keyboardVM.clickButton = clickButton; //拨号键点击
     keyboardVM.callPhoneNumber = callPhoneNumber; //拨打电话
-
-    keyboardVM.breakPhone = breakPhone; //挂掉电话
 
     function clickButton(num) {
       keyboardVM.callNumber = keyboardVM.callNumber + num;
@@ -33,17 +30,22 @@
     function callPhoneNumber(phone_number) {
       $window.location.href = "tel:" + phone_number;
       var times = 0;
-      keyboardVM.onCalling = true;
       keyboardVM.timer = '00:00:00';
-      var test = $interval(function () {
+      keyboardVM.onCalling = $interval(function () {
         times++;
         keyboardVM.timer = (parseInt(times / 3600) < 10 ? '0' + parseInt(times / 3600) : parseInt(times / 3600)) + ':' + (parseInt((times % 3600) / 60) < 10 ? '0' + parseInt((times % 3600) / 60) : parseInt((times % 3600) / 60)) + ':' + ((times % 3600) % 60 < 10 ? '0' + (times % 3600) % 60 : (times % 3600) % 60);
       }, 1000);// 表示每一秒执行一次，执行三次
-    }
 
-    function breakPhone() {
-      console.log('挂断电话');
-      $interval.cancel('test');
+      var alertPopup = $ionicPopup.alert({
+        title: '通话中',
+        template: phone_number,
+        okText: '挂断'
+      });
+      //结束通话
+      alertPopup.then(function (res) {
+        $interval.cancel(keyboardVM.onCalling);
+        console.log('通话时间：' + keyboardVM.timer);
+      });
     }
 
   }
