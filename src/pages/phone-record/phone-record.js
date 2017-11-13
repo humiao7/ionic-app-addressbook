aa5cf448822abb9ea8b9ec87332386589458bed3
humiao <1124003166@qq.com>
2017-11-13 17:29:39 +0800

    点击通话记录直接拨打电话

--- src/pages/phone-record/phone-record.js
+++ src/pages/phone-record/phone-record.js
@@ -7,9 +7,9 @@
     .module('applicationModule')
     .controller('phoneRecordCtrl', phoneRecordCtrl);
 
-  phoneRecordCtrl.$inject = ['$scope', '$window'];
+  phoneRecordCtrl.$inject = ['$scope', '$window', '$interval', '$ionicPopup'];
 
-  function phoneRecordCtrl($scope, $window) {
+  function phoneRecordCtrl($scope, $window, $interval, $ionicPopup) {
     var phoneRecordVM = this;
     // 对象----------
     phoneRecordVM.search = ''; // 搜索参数
@@ -31,6 +31,39 @@
 
     function callPhone(record) {
       $window.location.href = "tel:" + record.phone_num;
+      // 统计通话时长
+      var times = 0;
+      var timer = '00:00:00';
+      var onCalling = $interval(function () {
+        times++;
+        timer = (parseInt(times / 3600) < 10 ? '0' + parseInt(times / 3600) : parseInt(times / 3600)) + ':' + (parseInt((times % 3600) / 60) < 10 ? '0' + parseInt((times % 3600) / 60) : parseInt((times % 3600) / 60)) + ':' + ((times % 3600) % 60 < 10 ? '0' + (times % 3600) % 60 : (times % 3600) % 60);
+      }, 1000);// 表示每一秒执行一次
+      var alertPopup = $ionicPopup.alert({
+        title: '通话中',
+        template: record.phone_num,
+        okText: '挂断'
+      });
+
+      //结束通话
+      alertPopup.then(function (res) {
+        $interval.cancel(onCalling);
+        console.log('通话时间：' + timer);
+        var str_hours = parseInt(times / 3600) == 0 ? '' : (parseInt(times / 3600) + '时');
+        var str_minis = parseInt((times % 3600) / 60) == 0 ? '' : (parseInt((times % 3600) / 60) + '分');
+        var str_second = (times % 3600) % 60 == 0 ? '' : ((times % 3600) % 60 + '秒');
+        var date_now = new Date();
+
+        if (window.localStorage.phoneRecordData) {
+          var temp = JSON.parse(window.localStorage.phoneRecordData || '{}');
+        }
+        temp.push({
+          contact: record.contact,
+          status: '呼出' + str_hours + str_minis + str_second,
+          time: date_now.getFullYear() + '-' + (date_now.getMonth() + 1) + '-' + date_now.getDate(),
+          phone_num: record.phone_num
+        });
+        window.localStorage.phoneRecordData = JSON.stringify(temp);
+      });
     }
 
     function deleteRecord(index) {
